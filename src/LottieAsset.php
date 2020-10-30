ac1c44a2963ebd5ad50707c5b4ab6dccee649c6a
Naumenko Oleksandr <naumenko.oy@gbksoft.com>
2020-10-30 20:16:51 +0200

    edit asset

--- src/LottieAsset.php
+++ src/LottieAsset.php
@@ -8,6 +8,7 @@ use yii\web\AssetBundle;
 /**
  * Class LottieAsset
  * @see https://github.com/LottieFiles/lottie-player
+ * @see https://github.com/dotlottie/player-component#properties  - temp solution
  * @package alexnaumenko\lottie
  */
 class LottieAsset extends AssetBundle
@@ -15,8 +16,8 @@ class LottieAsset extends AssetBundle
     public $sourcePath = '@npm/dotlottie--player-component';
 
     public $js = [
-        "dist/dotlottie-player.js",
-        #"https://unpkg.com/@dotlottie/player-component@0.1.2/dist/dotlottie-player.js"
+        #"dist/dotlottie-player.js",
+        "https://unpkg.com/@dotlottie/player-component@0.1.2/dist/dotlottie-player.js" # tempered solution
     ];
 
     public $depends = [
